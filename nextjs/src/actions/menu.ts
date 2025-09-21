54492078ea5b1600014792baeffa71e4d74decc8
Abdallah Abdoulhakim <aabdoulhakim@gmail.com>
2025-09-21 15:15:12 +0300

    Publications single page for Informations rapides finished!

--- nextjs/src/actions/menu.ts
+++ nextjs/src/actions/menu.ts
@@ -40,8 +40,9 @@ export const fetchMenu = async (type: "MAIN" | "FOOTER") => {
 };
 
 
-export const fetchSubMenuByUrl = async (url:string)=>{
-  let breadCrumb : {id:string, label:string, description:string, url:string}[] =[]
+export const fetchSubMenuByUrl = async (url:string)  : Promise<{breadcrumb:{id:string, label:string, description:string, url:string}[] , title?:string , 
+subtitle?: string , short?:string, type?:string, parutionNumber?:string; parutionDate?:string , printableUrl? : string, printableSize?:string,  dataUrl? : string, dataSize?:string; }>=>{
+  let breadcrumb : {id:string, label:string, description:string, url:string}[] =[]
 
   const pathParts = url.split("/").filter((part) => part.length > 0);
 
@@ -78,43 +79,55 @@ export const fetchSubMenuByUrl = async (url:string)=>{
   })
 
 
-
-  if(!subMenu) return [];
+  if(!subMenu) return {breadcrumb:[]};
 
   if(type === "news"){
-    breadCrumb.push({id:subMenu.id, label:subMenu.label, description:subMenu?.description || "", url:subMenu.url})
+    breadcrumb.push({id:subMenu.id, label:subMenu.label, description:subMenu?.description || "", url:subMenu.url})
 
     if(pathParts.length > 1){
        const subCategory = subMenu.children.find(subElt=> subElt.url === `/${type}/${pathParts[1]}`)
        if(subCategory){
-        breadCrumb.push({id:subCategory.id, label:subCategory.label, description:subCategory?.description || "", url:subCategory.url})
+        breadcrumb.push({id:subCategory.id, label:subCategory.label, description:subCategory?.description || "", url:subCategory.url})
        }
     }
 
     if(pathParts.length > 2 ){
         const articleElt = await fetchArticleTitleBySlug(pathParts[2])
         if(articleElt){
-          breadCrumb.push({id:articleElt.id, label:articleElt.title, url:`/${type}/${pathParts[1]}/${articleElt.slug}`, description:""})
+          breadcrumb.push({id:articleElt.id, label:articleElt.title, url:`/${type}/${pathParts[1]}/${articleElt.slug}`, description:""})
         }
     }
+    
+
+     return {breadcrumb:breadcrumb , title:breadcrumb.at(-1)?.label, subtitle:breadcrumb.at(-1)?.description, type:"news"}
   }
 
   if(type==="publications"){
 
-    breadCrumb.push({id:subMenu.id, label:subMenu.label, description:subMenu?.description || "", url:subMenu.url})
+    breadcrumb.push({id:subMenu.id, label:subMenu.label, description:subMenu?.description || "", url:subMenu.url})
 
     if(pathParts.length > 1){
        const subCategory = subMenu.children.find(subElt=> subElt.url === `/${type}/${pathParts[1]}`)
        if(subCategory){
-        breadCrumb.push({id:subCategory.id, label:subCategory.label, description:subCategory?.description || "", url:subCategory.url})
+        breadcrumb.push({id:subCategory.id, label:subCategory.label, description:subCategory?.description || "", url:subCategory.url})
        }
     }
+
+    if (pathParts.length > 2){
+      const publicationElt = await fetchPublicationDetailsBySlug(pathParts[2])
+      
+      if (publicationElt){
+        breadcrumb.push({id:publicationElt.id, label:publicationElt.title, url:`/${pathParts[1]}/${publicationElt.slug}`, description:publicationElt.title})
+
+         return {breadcrumb:breadcrumb , title:publicationElt.short,  parutionNumber:publicationElt.parutionNumber, 
+          printableUrl:publicationElt.printableUrl ? `${process.env.NEXT_PUBLIC_STRAPI_BASE_URL}${publicationElt.printableUrl}` : "", dataUrl:publicationElt.dataUrl ? `${process.env.NEXT_PUBLIC_STRAPI_BASE_URL}${publicationElt.dataUrl}` : "", type: publicationElt.type, subtitle:publicationElt.title, short:publicationElt.abstract, parutionDate:publicationElt.parutionDate, dataSize:publicationElt.dataSize, printableSize:publicationElt.printableSize }
+      }
+    }
+
+      return {breadcrumb:breadcrumb , title:breadcrumb.at(-1)?.label, subtitle:breadcrumb.at(-1)?.description, type:"publication"}
   }
 
-
-
-
-  return breadCrumb
+   return {breadcrumb:breadcrumb , title:breadcrumb.at(-1)?.label, subtitle:breadcrumb.at(-1)?.description, type:"default"}
 
 }
 
@@ -135,4 +148,37 @@ export const fetchArticleTitleBySlug = async (url:string)=>{
     slug:articlesList[0].slug
   }
  
+}
+
+export const fetchPublicationDetailsBySlug = async (url:string)=>{
+  const publications = client.collection("publications");
+  const {data: publicationsList} = await publications.find({
+    filters:{
+      publicationSlug:url
+    },
+    fields:["type","short", "title", "abstract", "publicationSlug", "parutionNumber", "parutionDate"],
+    populate:{
+      data:{
+        fields:["name", "url", "size"]
+      },
+      printable:{
+        fields:["name", "url", "size"]
+      }
+    }
+  })
+
+  return {
+    id:publicationsList[0].documentId,
+    type:publicationsList[0].type,
+    short:publicationsList[0].short,
+    slug:publicationsList[0].publicationSlug,
+    title: truncateString(publicationsList[0].title, 110),
+    abstract:truncateString(publicationsList[0].abstract,500),
+    parutionNumber:publicationsList[0].parutionNumber,
+    parutionDate:publicationsList[0].parutionDate,
+    printableUrl:publicationsList[0].printable?.url,
+    dataUrl:publicationsList[0].data?.url,
+    dataSize : publicationsList[0].data?.size,
+    printableSize : publicationsList[0].printable?.size,
+  }
 }
--- nextjs/src/components/public/PageHeader.tsx
+++ nextjs/src/components/public/PageHeader.tsx
@@ -4,24 +4,69 @@ import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import { fetchSubMenuByUrl } from "@/actions/menu";
 import DotLoading from "@/components/base/DotLoading";
+import Link from "next/link";
+import { BsFilePdf } from "react-icons/bs";
+import { BsFiletypeXlsx } from "react-icons/bs";
+import byteSize from "byte-size";
 
 export default function PageHeader() {
   const [loading, setLoading] = useState(true);
-  const [breadcrumb, setBreadCrumb] = useState<
-    {
+
+  const [heading, setHeading] = useState<{
+    title?: string;
+    subtitle?: string;
+    short?: string;
+    parutionDate?: string;
+    parutionNumber?: string;
+    printableUrl?: string;
+    printableSize?: string;
+    dataUrl?: string;
+    dataSize?: string;
+    type?: string;
+    breadcrumb: {
       id: string;
       label: string;
       description: string;
       url: string;
-    }[]
-  >([]);
+    }[];
+  }>();
 
   const pathname = usePathname();
 
   useEffect(() => {
     async function loadData() {
       const breadCrumbresponse = await fetchSubMenuByUrl(pathname);
-      setBreadCrumb(breadCrumbresponse);
+
+      if (!breadCrumbresponse) return;
+
+      const {
+        breadcrumb,
+        title,
+        subtitle,
+        short,
+        parutionDate,
+        parutionNumber,
+        printableUrl,
+        printableSize,
+        dataUrl,
+        dataSize,
+        type,
+      } = breadCrumbresponse;
+
+      setHeading({
+        breadcrumb,
+        title: title,
+        subtitle: subtitle,
+        short: short,
+        parutionDate,
+        parutionNumber,
+        printableUrl,
+        printableSize,
+        dataSize,
+        dataUrl,
+        type,
+      });
+
       setLoading(false);
     }
 
@@ -33,7 +78,7 @@ export default function PageHeader() {
       className={
         loading
           ? "flex flex-col items-center justify-center"
-          : breadcrumb.length > 0
+          : heading
           ? "flex flex-col"
           : "hidden"
       }
@@ -44,22 +89,129 @@ export default function PageHeader() {
         <>
           <div className="py-15 flex items-center min-h-[20vh] relative bg-primary">
             <div className="container mx-auto relative flex flex-col items-center justify-center">
-              <div className="flex flex-col justify-center">
-                <div className="px-5 md:px-0  text-center">
-                  <h2 className="text-2xl lg:text-5xl font-[500] text-white font-poppins">
-                    {breadcrumb.at(-1)?.label}
-                  </h2>
+              {heading && heading.type === "Informations Rapides" ? (
+                <div className="w-full grid grid-cols-1 md:grid-cols-[2fr_1fr]">
+                  <div className="py-5 px-2 flex flex-col space-y-5">
+                    {heading.title && (
+                      <h1
+                        className={`${
+                          heading.title.length > 80
+                            ? "text-xl lg:text-3xl"
+                            : "text-2xl lg:text-5xl"
+                        } font-[600] text-white font-poppins`}
+                      >
+                        {heading.title}
+                      </h1>
+                    )}
+                    {heading.subtitle && (
+                      <h2 className="font-semibold text-[18px] text-white font-poppins">
+                        {heading.subtitle}
+                      </h2>
+                    )}
+                    {heading.short && (
+                      <p className="text-white text-sm md:text-[15px]">
+                        {heading.short}
+                      </p>
+                    )}
+                  </div>
+                  <div className="flex flex-col space-y-3 p-3">
+                    <h2 className="text-white/80 uppercase font-bold">
+                      {heading.type}
+                    </h2>
+                    {heading.parutionNumber && (
+                      <p className="text-white text-[13px]">
+                        N° {heading.parutionNumber}
+                      </p>
+                    )}
+                    {heading.parutionDate && (
+                      <p className="text-white text-[13px]">
+                        Paru le :{" "}
+                        {new Date(heading.parutionDate).toLocaleDateString()}
+                      </p>
+                    )}
 
-                  {breadcrumb.at(-1)?.description && (
-                    <p className="text-white/80">
-                      {breadcrumb.at(-1)?.description}
-                    </p>
-                  )}
+                    {heading.printableUrl && (
+                      <div className="bg-white border border-primary hover:bg-white/80 rounded flex p-5 flex-col group cursor-pointer">
+                        <Link
+                          className="flex justify-end"
+                          href={heading.printableUrl}
+                        >
+                          <div className="flex flex-col justify-end items-end">
+                            <p className="text-primary text-[13px] uppercase group-hover:underline">
+                              Version Imprimable
+                            </p>
+                            <p className="text-neutral-500 text-[13px] group-hover:underline">
+                              {`(pdf ${
+                                !isNaN(Number(heading.printableSize))
+                                  ? `, ${byteSize(
+                                      Number(heading.printableSize) * 1024
+                                    )}`
+                                  : ""
+                              })`}
+                            </p>
+                          </div>
+                          <div className="ml-5">
+                            <BsFilePdf color="#008374" size={35} />
+                          </div>
+                        </Link>
+                      </div>
+                    )}
+                    {heading.dataUrl && (
+                      <div className="bg-white border border-primary hover:bg-white/80 rounded flex p-5 flex-col group cursor-pointer">
+                        <Link
+                          className="flex justify-end"
+                          href={heading.dataUrl}
+                        >
+                          <div className="flex  flex-col justify-end items-end">
+                            <p className="text-primary text-[13px] uppercase group-hover:underline">
+                              Données
+                            </p>
+                            <p className="text-neutral-500 text-[13px] group-hover:underline">
+                              {`(xlsx ${
+                                !isNaN(Number(heading.dataSize))
+                                  ? `, ${byteSize(
+                                      Number(heading.dataSize) * 1024
+                                    )}`
+                                  : ""
+                              })`}
+                            </p>
+                          </div>
+                          <div className="ml-5">
+                            <BsFiletypeXlsx color="#008374" size={35} />
+                          </div>
+                        </Link>
+                      </div>
+                    )}
+                  </div>
                 </div>
-              </div>
+              ) : (
+                <div className="flex flex-col justify-center">
+                  <div className="px-5 md:px-0  text-center">
+                    {heading?.title && (
+                      <h2
+                        className={`${
+                          heading.title.length > 80
+                            ? "text-xl lg:text-3xl"
+                            : "text-2xl lg:text-5xl"
+                        } font-[600] text-white font-poppins`}
+                      >
+                        {heading.title}
+                      </h2>
+                    )}
+
+                    {heading?.subtitle && (
+                      <p className="text-white text-justify">
+                        {heading.subtitle}
+                      </p>
+                    )}
+                  </div>
+                </div>
+              )}
             </div>
           </div>
-          <Breadcrumb breadcrumb={breadcrumb} />
+          {heading && heading.breadcrumb.length > 0 && (
+            <Breadcrumb breadcrumb={heading.breadcrumb} />
+          )}
         </>
       )}
     </div>
